e84c942a1020f636af1ee3212ccb957f6e6b6aca
Peter Burkholder <peterb@getchef.com>
2014-11-18 11:14:24 -0500

    0.2.0 attempt at motd server

--- dc_devfest/metadata.rb
+++ dc_devfest/metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'pburkholder@getchef.com'
 license          'Apache 2.0'
 description      'Installs/Configures dc_devfest'
 long_description 'Installs/Configures dc_devfest'
-version          '0.1.1'
+version          '0.2.0'
 
 depends 'gce', '~> 0.3.0'
+depends 'pdb_motd'
--- dc_devfest/recipes/default.rb
+++ dc_devfest/recipes/default.rb
@@ -5,3 +5,4 @@
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
 include_recipe 'dc_devfest::metaprovision'
+include_recipe 'dc_devfest::motd'
