2ca7495080c6ee7bc9dab3171afc700741f65bb2
Peter Burkholder <peterb@getchef.com>
2014-11-18 22:15:54 -0500

    Fixed naming

--- dc_devfest/recipes/default.rb
+++ dc_devfest/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
-include_recipe 'pdb_motd'
 include_recipe 'dc_devfest::dbserver'
-include_recipe 'dc_devfest::app-server'
-include_recipe 'dc_devfest::http-server'
+include_recipe 'dc_devfest::appserverA'
+include_recipe 'dc_devfest::appserverB'
+include_recipe 'dc_devfest::webserver'
